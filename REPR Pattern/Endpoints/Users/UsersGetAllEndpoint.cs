b1c10db90a9af0bb8d517d5dd70380e562a25122
Biswajit Panday <biswajitmailid@gmail.com>
2024-10-26 17:46:42 +0600

    Create UsersGetAllEndpoint.

---
+++ REPR Pattern/Endpoints/Users/UsersGetAllEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using REPR_Pattern.Repository;
+using REPR_Pattern.Repository.Interfaces;
+
+namespace REPR_Pattern.Endpoints.Users;
+
+[HttpGet("api/users")]
+[AllowAnonymous]
+public class UsersGetAllEndpoint(IUserRepository userRepository) : EndpointWithoutRequest<List<UserEntity>>
+{
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var users = await userRepository.GetAllUser();
+        await SendAsync(users, cancellation: ct);
+    }
+}
