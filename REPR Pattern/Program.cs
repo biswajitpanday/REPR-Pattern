ffbad555c1fa2f80cc15caf5ff4f5d25eb80eb40
Biswajit Panday <biswajitmailid@gmail.com>
2024-10-26 17:14:58 +0600

    Add UserRepository dependency, and Configure FastEndpoints.

--- REPR Pattern/Program.cs
+++ REPR Pattern/Program.cs
@@ -1,5 +1,13 @@
+using FastEndpoints;
+using REPR_Pattern.Repository;
+using REPR_Pattern.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddFastEndpoints();
+builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseFastEndpoints();
 app.Run();
